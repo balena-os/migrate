2e5c56c52288b123f0272f8c5ce68172b66d6aa7
Thomas <thomas@etnur.net>
2019-03-06 10:53:19 +1300

    using csv reader in sysinfo output

--- src/mswin/mod.rs
+++ src/mswin/mod.rs
@@ -2,10 +2,11 @@ use  crate::common::mig_error::{MigError,MigErrorCode};
 use  crate::common::SysInfo;
 
 use log::{trace,error};
-
+use std::error::Error;
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
 use std::vec::Vec;
+use csv;
 
 const OS: &str = "windows";
 
@@ -31,37 +32,28 @@ pub fn sys_info() -> Result<SysInfo,MigError> {
 
     // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
 
-    let mut out_str : String = String::from("");
-    let lines = match process.stdout {
+    let mut reader = match process.stdout {
         // TODO: how to return why as source source
         None => { return Err(MigError::from_code(MigErrorCode::ErrCmdIO, "failed to read command output from: powershell Systeminfo /FO CSV", None )) }
-        Some(mut std_out) => {            
-            match std_out.read_to_string(&mut out_str) {
-                Ok(_r) => { 
-                    let lines: Vec<&str> = out_str.lines().collect();
-                    lines 
-                }, 
-                Err(why) => { return Err(MigError::from_code(MigErrorCode::ErrCmdIO, "failed to read command output from: powershell Systeminfo /FO CSV", Some(Box::new(why)))) },
-                }
-            }
+        Some(std_out) => csv::Reader::from_reader(std_out)
         };
 
-    
-    match lines.len() {
+    let records: Vec<csv::Result<csv::StringRecord>> = reader.records().collect();
+    match records.len() {
         2 => (),            
         _ => { 
             error!("sys_info: too many lines in command output of  powershell Systeminfo /FO CSV");
-            for line in lines {
-                error!("sys_info: {}", line);
+            for record in records {
+                error!("sys_info: {:?}", record);
             }
             return Err(MigError::from_code(MigErrorCode::ErrInvParam, "unexpected number of output lines received from: powershell Systeminfo /FO CSV", None)); 
         }
     }
 
-    let headers = lines[0];
-    let data = lines[1];
-    trace!("sys_info: headers: {}",headers);
-    trace!("sys_info: data:    {}",data);
+    let headers = &records[0];
+    let data = &records[1];
+    trace!("sys_info: headers: {:?}",headers);
+    trace!("sys_info: data:    {:?}",data);
 
     Ok(SysInfo::new("windows"))
     } else {
