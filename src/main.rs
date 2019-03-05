b4eba41d768162f7b5c3371ffc47eb83dc464d8f
Thomas <thomas@etnur.net>
2019-03-06 10:18:16 +1300

    added debug logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use win_test::mswin;
 
+#[macro_use]
+extern crate log;
+extern crate stderrlog;
+
 fn main() {
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(6)
+        .timestamp(stderrlog::Timestamp::Millisecond)
+        .init()
+        .unwrap();
+
+    trace!("wint_test started");
     if mswin::available() {
         mswin::process().unwrap()
     } else {
--- src/mswin/mod.rs
+++ src/mswin/mod.rs
@@ -1,7 +1,8 @@
 use  crate::common::mig_error::{MigError,MigErrorCode};
 use  crate::common::SysInfo;
 
-use std::error::Error;
+use log::{trace,error};
+
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
 use std::vec::Vec;
@@ -9,17 +10,22 @@ use std::vec::Vec;
 const OS: &str = "windows";
 
 pub fn available() -> bool {    
+    trace!("called available()");
     return cfg!(windows);
 }
 
 pub fn sys_info() -> Result<SysInfo,MigError> {
+    trace!("called sys_info()");
     if cfg!(windows) {
     // Spawn the command `powershell Systeminfo /FO CSV`
     let process = match Command::new("powershell.exe")
                                 .args(&["Systeminfo", "/FO", "CSV"])
                                 .stdout(Stdio::piped())
+                                .stderr(Stdio::piped())
                                 .spawn() {
-        Err(why) => { return Err(MigError::from_code(MigErrorCode::ErrExecProcess, "failed to execute: powershell Systeminfo /FO CSV", Some(Box::new(why)))) },
+        Err(why) => { 
+            // TODO: extract stderr & add to returned error
+            return Err(MigError::from_code(MigErrorCode::ErrExecProcess, "failed to execute: powershell Systeminfo /FO CSV", Some(Box::new(why)))) },
         Ok(process) => process,
     };
 
@@ -31,8 +37,7 @@ pub fn sys_info() -> Result<SysInfo,MigError> {
         None => { return Err(MigError::from_code(MigErrorCode::ErrCmdIO, "failed to read command output from: powershell Systeminfo /FO CSV", None )) }
         Some(mut std_out) => {            
             match std_out.read_to_string(&mut out_str) {
-                Ok(r) => { 
-                    println!("powershell Systeminfo /FO CSV:\n{}", out_str);
+                Ok(_r) => { 
                     let lines: Vec<&str> = out_str.lines().collect();
                     lines 
                 }, 
@@ -43,12 +48,20 @@ pub fn sys_info() -> Result<SysInfo,MigError> {
 
     
     match lines.len() {
-        2 => (),
-        _ => return Err(MigError::from_code(MigErrorCode::ErrInvParam, "unexpected number of output lines received from: powershell Systeminfo /FO CSV", None)) 
+        2 => (),            
+        _ => { 
+            error!("sys_info: too many lines in command output of  powershell Systeminfo /FO CSV");
+            for line in lines {
+                error!("sys_info: {}", line);
+            }
+            return Err(MigError::from_code(MigErrorCode::ErrInvParam, "unexpected number of output lines received from: powershell Systeminfo /FO CSV", None)); 
+        }
     }
 
     let headers = lines[0];
     let data = lines[1];
+    trace!("sys_info: headers: {}",headers);
+    trace!("sys_info: data:    {}",data);
 
     Ok(SysInfo::new("windows"))
     } else {
