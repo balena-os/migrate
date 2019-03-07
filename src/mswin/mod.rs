6176763d0044bca886a60741e50081f4073fa740
Thomas Runte <thomasr@balena.io>
2019-03-08 01:23:33 +1300

    added cmd line options

--- src/mswin/mod.rs
+++ src/mswin/mod.rs
@@ -1,6 +1,8 @@
 use crate::common::mig_error::MigError;
+use crate::common::SysInfo;
 
 use log::{info, trace};
+ 
 // use std::error::Error;
 // use std::io::prelude::*;
 
@@ -11,6 +13,13 @@ mod powershell;
 pub fn available() -> bool {
     trace!("called available()");
     return cfg!(windows);
+}
+
+pub fn get_sys_info() -> Result<Box<SysInfo>,MigError> {
+    match powershell::PSInfo::try_init() {
+        Ok(si) => Ok(Box::new(si)),
+        Err(why) => Err(why)
+    }
 }
 
 pub fn process() -> Result<(), MigError> {
