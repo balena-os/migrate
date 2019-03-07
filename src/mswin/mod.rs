e8b85ecf17241cc139ce695df7b4488d35fb1d84
Thomas <thomas@etnur.net>
2019-03-07 15:07:02 +1300

    moved code to powershell

--- src/mswin/mod.rs
+++ src/mswin/mod.rs
@@ -1,20 +1,10 @@
-use crate::common::mig_error::{MigError, MigErrorCode};
-use crate::common::SysInfo;
+use crate::common::mig_error::MigError;
 
-use log::{error, info, trace};
+use log::{info, trace};
 // use std::error::Error;
 // use std::io::prelude::*;
 
-use csv;
-use std::collections::HashMap;
-use std::io::ErrorKind;
-use std::process::{Command, Stdio};
-use std::vec::Vec;
-
 mod powershell;
-
-const POWERSHELL: &str = "powershell.exe";
-const POWERSHELL_PARAMS: [&'static str; 3] = ["Systeminfo", "/FO", "CSV"];
 
 // const OS: &str = "windows";
 
@@ -23,133 +13,8 @@ pub fn available() -> bool {
     return cfg!(windows);
 }
 
-pub fn sys_info() -> Result<SysInfo, MigError> {
-    trace!("called sys_info()");
-    if powershell::available() {
-        if cfg!(windows) {
-            // Spawn the command `powershell Systeminfo /FO CSV`
-            let process = match Command::new(POWERSHELL)
-                .args(&POWERSHELL_PARAMS)
-                .stdout(Stdio::piped())
-                .stderr(Stdio::piped())
-                .spawn()
-            {
-                Ok(process) => process,
-                Err(why) => match why.kind() {
-                    ErrorKind::NotFound => {
-                        return Err(MigError::from_code(
-                            MigErrorCode::ErrPgmNotFound,
-                            "failed to execute: powershell Systeminfo /FO CSV",
-                            Some(Box::new(why)),
-                        ));
-                    }
-                    _ => {
-                        return Err(MigError::from_code(
-                            MigErrorCode::ErrExecProcess,
-                            "failed to execute: powershell Systeminfo /FO CSV",
-                            Some(Box::new(why)),
-                        ));
-                    }
-                },
-            };
-
-            let mut reader = match process.stdout {
-                Some(std_out) => csv::Reader::from_reader(std_out),
-                None => {
-                    return Err(MigError::from_code(
-                        MigErrorCode::ErrCmdIO,
-                        "failed to read command output from: powershell Systeminfo /FO CSV",
-                        None,
-                    ));
-                }
-            };
-
-            let records: Vec<csv::Result<csv::StringRecord>> = reader.records().collect();
-            match records.len() {
-                1 => (),
-                _ => {
-                    error!("sys_info: invalid number of records () in command output of  powershell Systeminfo /FO CSV");
-                    for record in records {
-                        error!("sys_info: {:?}", record);
-                    }
-                    return Err(MigError::from_code(MigErrorCode::ErrInvParam, "unexpected number of output lines received from: powershell Systeminfo /FO CSV", None));
-                }
-            }
-
-            let headers = match reader.headers() {
-                Ok(sr) => {
-                    let hdrs: Vec<&str> = sr.iter().collect();
-                    hdrs
-                }
-                Err(_why) => return Err(MigError::from_code(
-                    MigErrorCode::ErrInvParam,
-                    "no headers found in output lines received from: powershell Systeminfo /FO CSV",
-                    None,
-                )), // Some(Box::new(why))))
-            };
-
-            let data = match &records[0] {
-                Ok(sr) => {
-                    let dt: Vec<&str> = sr.iter().collect();
-                    dt
-                }
-                Err(_why) => return Err(MigError::from_code(
-                    MigErrorCode::ErrInvParam,
-                    "no data found in output lines received from: powershell Systeminfo /FO CSV",
-                    None,
-                )), // Some(Box::new(why))))
-            };
-
-            trace!("sys_info: headers: {:?}", headers);
-            trace!("sys_info: data:    {:?}", data);
-
-            let mut sys_info_map: HashMap<String, String> = HashMap::new();
-            let columns = headers.len();
-
-            for idx in 0..columns {
-                let hdr: &str = &headers[idx];
-                let data_str = match data.get(idx) {
-                    Some(s) => s,
-                    None => "",
-                };
-                trace!("sys_info: adding {} ->  {}", hdr, data_str);
-                sys_info_map.insert(String::from(hdr), String::from(data_str));
-            }
-
-            let mut s_info = SysInfo::new(sys_info_map);
-            s_info.set_os_name("OS Name")?;
-            s_info.set_os_version("OS Version")?;
-            s_info.set_host_name("Host Name")?;
-            s_info.set_tot_mem("Total Physical Memory")?;
-            s_info.set_avail_mem("Available Physical Memory")?;
-
-            Ok(s_info)
-        } else {
-            Err(MigError::from_code(
-                MigErrorCode::ErrInvOSType,
-                "invalid OS, not windows",
-                None,
-            ))
-        }
-    } else {
-        Err(MigError::from_code(
-            MigErrorCode::ErrFeatureMissing,
-            "powershell not is available on windows",
-            None,
-        ))
-    }
-}
-
 pub fn process() -> Result<(), MigError> {
-    let s_info = sys_info()?;
+    let s_info = powershell::sys_info()?;
     info!("process: os_type = {}", s_info.get_os_name());
     Ok(())
-}
-
-#[cfg(test)]
-#[test]
-fn get_sys_info() {
-    let s_info = sys_info().unwrap();
-    let os_name = s_info.get_os_name();
-    assert!(os_name.len() > 0);
 }
--- src/mswin/powershell.rs
+++ src/mswin/powershell.rs
@@ -5,24 +5,27 @@ pub const POWERSHELL_SYSINFO_PARAMS: [&'static str; 3] = ["Systeminfo", "/FO", "
 pub const POWERSHELL_VERSION_PARAMS: [&'static str; 1] = ["$PSVersionTable.PSVersion"];
 
 use crate::common::mig_error::{MigError, MigErrorCode};
+use crate::common::SysInfo;
 
-use log::{ warn, trace};
+use csv;
+use log::{error, trace, warn};
+use std::collections::HashMap;
 use std::io::ErrorKind;
 use std::io::Read;
 use std::process::{Command, Stdio};
 
-static mut POWERSHELL_VERSION: Option<(u32,u32)> = None;
+static mut POWERSHELL_VERSION: Option<(u32, u32)> = None;
 
 pub fn available() -> bool {
     trace!("{}::available(): called available()", MODULE);
 
     match get_ps_ver() {
         Ok(_v) => true,
-        Err(_why) => false
+        Err(_why) => false,
     }
 }
 
-pub fn get_ps_ver() -> Result<(u32,u32),MigError>  {
+pub fn get_ps_ver() -> Result<(u32, u32), MigError> {
     trace!("{}::get_ps_ver(): called", MODULE);
 
     // TODO: add mutex
@@ -30,13 +33,13 @@ pub fn get_ps_ver() -> Result<(u32,u32),MigError>  {
     unsafe {
         match &POWERSHELL_VERSION {
             Some(s) => return Ok(s.clone()),
-            None => () 
+            None => (),
         }
     }
 
     let output = call_to_string(&POWERSHELL_VERSION_PARAMS)?;
-     
-    let lines : Vec<&str> = output.stdout.lines().collect();
+
+    let lines: Vec<&str> = output.stdout.lines().collect();
     match lines.len() {
         3 => (),
         0 => {
@@ -49,28 +52,109 @@ pub fn get_ps_ver() -> Result<(u32,u32),MigError>  {
         _ => return Err(MigError::from_code(MigErrorCode::ErrInvParam, &format!("{}::available(): unexpected number of ouput lines in powershell version output: {}", MODULE, output.stdout),None))            
     }
 
-    let headers : Vec<&str> = lines[0].split_whitespace().collect();
-    let values : Vec<&str> = lines[2].split_whitespace().collect();
+    let headers: Vec<&str> = lines[0].split_whitespace().collect();
+    let values: Vec<&str> = lines[2].split_whitespace().collect();
 
-    let mut major : u32 = 1;
-    let mut minor : u32 = 0;
+    let mut major: u32 = 1;
+    let mut minor: u32 = 0;
 
-    for idx in 0..headers.len() {        
+    for idx in 0..headers.len() {
         let hdr: &str = &headers[idx];
         match hdr {
-            "Major" => { major = values.get(idx).unwrap().parse().unwrap(); }
-            "Minor" => { minor = values.get(idx).unwrap().parse().unwrap(); }
-            _ => { break; }
+            "Major" => {
+                major = values.get(idx).unwrap().parse().unwrap();
+            }
+            "Minor" => {
+                minor = values.get(idx).unwrap().parse().unwrap();
+            }
+            _ => {
+                break;
             }
         }
+    }
 
     unsafe {
         POWERSHELL_VERSION = Some((major, minor));
     }
 
-    Ok((major,minor))    
+    Ok((major, minor))
 }
 
+pub fn sys_info() -> Result<SysInfo, MigError> {
+    trace!("{}::sysy_info(): called", MODULE);
+    if available() {
+        let output = call_to_string(&POWERSHELL_SYSINFO_PARAMS)?;
+        let mut reader = csv::Reader::from_reader(output.stdout.as_bytes());
+        let records: Vec<csv::Result<csv::StringRecord>> = reader.records().collect();
+        match records.len() {
+            1 => (),
+            _ => {
+                error!("sys_info: invalid number of records () in command output of  powershell Systeminfo /FO CSV");
+                for record in records {
+                    error!("sys_info: {:?}", record);
+                }
+                return Err(MigError::from_code(MigErrorCode::ErrInvParam, &format!("{}::sys_info: unexpected number of output lines received from: powershell Systeminfo /FO CSV",MODULE), None));
+            }
+        }
+
+        let headers =
+            match reader.headers() {
+                Ok(sr) => {
+                    let hdrs: Vec<&str> = sr.iter().collect();
+                    hdrs
+                }
+                Err(_why) => return Err(MigError::from_code(
+                    MigErrorCode::ErrInvParam,
+                    &format!("{}::sys_info: no headers found in output lines received from: powershell Systeminfo /FO CSV",MODULE),
+                    None,
+                )), // Some(Box::new(why))))
+            };
+
+        let data =
+            match &records[0] {
+                Ok(sr) => {
+                    let dt: Vec<&str> = sr.iter().collect();
+                    dt
+                }
+                Err(_why) => return Err(MigError::from_code(
+                    MigErrorCode::ErrInvParam,
+                    &format!("{}::sys_info: no data found in output lines received from: powershell Systeminfo /FO CSV",MODULE),
+                    None,
+                )), // Some(Box::new(why))))
+            };
+
+        trace!("sys_info: headers: {:?}", headers);
+        trace!("sys_info: data:    {:?}", data);
+
+        let mut sys_info_map: HashMap<String, String> = HashMap::new();
+        let columns = headers.len();
+
+        for idx in 0..columns {
+            let hdr: &str = &headers[idx];
+            let data_str = match data.get(idx) {
+                Some(s) => s,
+                None => "",
+            };
+            trace!("sys_info: adding {} ->  {}", hdr, data_str);
+            sys_info_map.insert(String::from(hdr), String::from(data_str));
+        }
+
+        let mut s_info = SysInfo::new(sys_info_map);
+        s_info.set_os_name("OS Name")?;
+        s_info.set_os_version("OS Version")?;
+        s_info.set_host_name("Host Name")?;
+        s_info.set_tot_mem("Total Physical Memory")?;
+        s_info.set_avail_mem("Available Physical Memory")?;
+
+        Ok(s_info)
+    } else {
+        Err(MigError::from_code(
+            MigErrorCode::ErrFeatureMissing,
+            &format!("{}::sys_info: powershell not is available on windows",MODULE),
+            None,
+        ))
+    }
+}
 
 pub struct PWRes {
     stdout: String,
@@ -89,14 +173,14 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
             ErrorKind::NotFound => {
                 return Err(MigError::from_code(
                     MigErrorCode::ErrPgmNotFound,
-                    "failed to execute: powershell Systeminfo /FO CSV",
+                    &format!("{}::call_to_string: failed to execute: powershell Systeminfo /FO CSV",MODULE),
                     Some(Box::new(why)),
                 ));
             }
             _ => {
                 return Err(MigError::from_code(
                     MigErrorCode::ErrExecProcess,
-                    "failed to execute: powershell Systeminfo /FO CSV",
+                    &format!("{}::call_to_string: failed to execute: powershell Systeminfo /FO CSV",MODULE),
                     Some(Box::new(why)),
                 ));
             }
@@ -110,7 +194,7 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
             Err(why) => {
                 return Err(MigError::from_code(
                     MigErrorCode::ErrCmdIO,
-                    "failed to read command output from: powershell Systeminfo /FO CSV",
+                    &format!("{}::call_to_string: failed to read command output from: powershell Systeminfo /FO CSV",MODULE),
                     Some(Box::new(why)),
                 ));
             }
@@ -118,7 +202,7 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
         None => {
             return Err(MigError::from_code(
                 MigErrorCode::ErrCmdIO,
-                "failed to read command output from: powershell Systeminfo /FO CSV",
+                &format!("{}::call_to_string: failed to read command output from: powershell Systeminfo /FO CSV",MODULE),
                 None,
             ));
         }
@@ -131,7 +215,7 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
             Err(why) => {
                 return Err(MigError::from_code(
                     MigErrorCode::ErrCmdIO,
-                    "failed to read command error output from: powershell Systeminfo /FO CSV",
+                    &format!("{}::call_to_string: failed to read command error output from: powershell Systeminfo /FO CSV",MODULE),
                     Some(Box::new(why)),
                 ));
             }
@@ -139,7 +223,7 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
         None => {
             return Err(MigError::from_code(
                 MigErrorCode::ErrCmdIO,
-                "failed to read command error output from: powershell Systeminfo /FO CSV",
+                &format!("{}::call_to_string: failed to read command error output from: powershell Systeminfo /FO CSV",MODULE),
                 None,
             ));
         }
@@ -149,4 +233,16 @@ fn call_to_string(args: &[&str]) -> Result<PWRes, MigError> {
         stdout: stdout_str,
         stderr: stderr_str,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_sys_info() {
+        let s_info = sys_info().unwrap();
+        let os_name = s_info.get_os_name();
+        assert!(os_name.len() > 0);
+    }
 }
