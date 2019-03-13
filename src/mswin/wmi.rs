a2b1c55530d93163c1cd1ab8b1b75da7bdc38263
Thomas <thomas@etnur.net>
2019-03-14 11:01:55 +1300

    update mswin/wmi.rs

--- src/mswin/wmi.rs
+++ src/mswin/wmi.rs
@@ -1,65 +1,16 @@
-extern crate winapi;
+extern crate wmi;
 
-use crate::common::mig_error::{MigError,MigErrorCode};
+pub fn wmi_query() -> Result<i32, Box<std::error::Error>> {    
+    use wmi::{COMLibrary, WMIConnection};
+    let com_con = COMLibrary::new().unwrap();
+    let wmi_con = WMIConnection::new(com_con.into()).unwrap();
 
-use log::{trace};
+    use std::collections::HashMap;
+    use wmi::Variant;
+    let results: Vec<HashMap<String, Variant>> = wmi_con.raw_query("SELECT * FROM Win32_OperatingSystem").unwrap();
 
-use std::ptr::null_mut;
-use winapi::um::combaseapi::{CoInitializeSecurity, CoUninitialize};
-use winapi::um::objbase::{CoInitialize};
-use winapi::um::handleapi::{INVALID_HANDLE_VALUE};
-use winapi::um::objidl::{EOAC_NONE};
-use lazy_static::lazy_static;
-
-const MODULE: &str = "wmi";
-
-const S_OK: i32 = 0;
-// TODO: retrieve definitions
-const RPC_C_AUTHN_LEVEL_DEFAULT: u32 = 0;
-const RPC_C_IMP_LEVEL_IMPERSONATE: u32 = 3;
-
-pub struct ComHandle {
-    h_com: i32,
-}
-
-impl ComHandle {
-    pub fn try_init() -> Result<ComHandle,MigError> {
-        trace!("{}::try_init: entered",MODULE);
-
-        let h_com = unsafe { CoInitialize(null_mut()) };
-        if h_com != S_OK {
-            return Err(MigError::from_code(MigErrorCode::ErrNotImpl, &format!("{}::try_init: failed to initialize COM interface: rc={}",MODULE,h_com), None));    
-        } 
-
-        trace!("{}::try_init: com initialized, init security",MODULE);
-        let res =  unsafe { CoInitializeSecurity(
-                null_mut(),                 // Security descriptor    
-                -1,                         // COM negotiates authentication service
-                null_mut(),                 // Authentication services
-                null_mut(),                 // Reserved
-                RPC_C_AUTHN_LEVEL_DEFAULT,  // Default authentication level for proxies
-                RPC_C_IMP_LEVEL_IMPERSONATE,// Default Impersonation level for proxies
-                null_mut(),                 // Authentication info
-                EOAC_NONE,                  // Additional capabilities of the client or server
-                null_mut()) };              // Reserved
-
-        if res != S_OK {
-            unsafe { CoUninitialize() };
-            return Err(MigError::from_code(MigErrorCode::ErrNotImpl, &format!("{}::try_init: failed to initialize COM interface Security: rc={}",MODULE,res), None));            
-        }        
-
-        trace!("{}::try_init: com initialized",MODULE);
-        
-
-        Ok(ComHandle{h_com: h_com})
+    for os in results {
+        println!("{:#?}", os);
     }
-}
-
-impl<'a> Drop for ComHandle + 'a {
-    fn drop(&mut self) {
-        if self.h_com != 0 {
-            self.h_com = 0;
-            unsafe { CoUninitialize() };
-        }
-    }
-}
+    Ok(0)
+}    
