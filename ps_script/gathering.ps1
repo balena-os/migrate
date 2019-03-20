0399b25dfd39e2f43832c4f5c1dd4c779fd5da81
Thomas Runte <thomasr@balena.io>
2019-03-21 07:26:00 +1300

    added ps script gathering

---
+++ ps_script/gathering.ps1
@@ -0,0 +1,12 @@
+﻿    [System.Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US';
+    # [System.Threading.Thread]::CurrentThread.CurrentCulture = 'en-US';
+    $query = “Select * from Win32_Bios”;
+    Get-WmiObject -Query $query;
+    $query = "SELECT Caption,Version,OSArchitecture, BootDevice, TotalVisibleMemorySize,FreePhysicalMemory FROM Win32_OperatingSystem"
+    Get-WmiObject -Query $query;
+    
+    Get-WmiObject -Class Win32_DiskPartition  | Select-Object -Property *;
+
+
+  
+
