fda7940909da7d9b478493d8be2b23ffa2bcbd1c
Thomas Runte <thomasr@balena.io>
2019-03-21 07:58:49 +1300

    messing with ps

--- ps_script/gathering.ps1
+++ ps_script/gathering.ps1
@@ -1,12 +1,13 @@
-﻿    [System.Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US';
+﻿[System.Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US';
     # [System.Threading.Thread]::CurrentThread.CurrentCulture = 'en-US';
     $query = “Select * from Win32_Bios”;
     Get-WmiObject -Query $query;
     $query = "SELECT Caption,Version,OSArchitecture, BootDevice, TotalVisibleMemorySize,FreePhysicalMemory FROM Win32_OperatingSystem"
     Get-WmiObject -Query $query;
     
-    Get-WmiObject -Class Win32_DiskPartition  | Select-Object -Property *;
+    # Get-WmiObject -Class Win32_DiskPartition  ;
 
+    # Get-WmiObject -Class Win32_DiskPartition  | Select-Object -Property *;
 
-  
-
+    $query = "SELECT Index,DiskIndex,Type,PrimaryPartition,Bootable,BootPartition,BlockSize,Size,StartingOffset  from Win32_DiskPartition"
+    Get-WmiObject -Query $query;
