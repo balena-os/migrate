25140061202b8127934da506ec242434098b2f73
Thomas Runte <thomas@etnur.net>
2019-09-24 11:51:39 +0200

    cosmetical changes in balena-extract, added rpi4 to mk_mig_config

--- src/bin/balena-extract.rs
+++ src/bin/balena-extract.rs
@@ -1,37 +1,3 @@
-/*
-if let MigMode::Extract = config.migrate.get_mig_mode() {
-if arg_matches.is_present("device-type") {
-if let Some(dev_type) = arg_matches.value_of("device-type") {
-config.migrate.set_extract_device(dev_type);
-}
-} else {
-error!("device-type option is mandatory for mode EXTRACT. Please specify a device type using the --device-type or -d option");
-return Err(MigError::displayed());
-}
-}
-
-            MigMode::Extract => {
-                if let None = self.work_dir {
-                    error!("A required parameter was not found: 'work_dir'");
-                    return Err(MigError::displayed());
-                }
-
-                if let None = self.extract_device {
-                    error!("A required parameter was not found: 'extract_device'");
-                    return Err(MigError::displayed());
-                }
-                Ok(())
-            }
-
-            MigMode::Extract => {
-                let mut extractor = Extractor::new(config)?;
-                extractor.extract(None)?;
-                Ok(())
-            }
-
-*/
-//use balena_migrate::{extract};
-
 #[cfg(target_os = "linux")]
 fn main() {
     use balena_migrate::{common::MigErrorKind, extract};
