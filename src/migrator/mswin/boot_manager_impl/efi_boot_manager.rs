cfa35db3fab3f7a408457841eb4676fdac05823e
Thomas Runte <thomas@etnur.net>
2019-10-12 09:50:42 -0700

    wip on mswin

--- src/migrator/mswin/boot_manager_impl/efi_boot_manager.rs
+++ src/migrator/mswin/boot_manager_impl/efi_boot_manager.rs
@@ -200,13 +200,12 @@ impl BootManager for EfiBootManager {
 
         // TODO: prefer PARTUUID to guessed device name
 
-        let os_api = OSApi::new()?;
         let startup_content = if let Some(ref partuuid) = boot_dev.part_uuid {
             format!(
                 "{}{} initrd={} root=PARTUUID={} rootfstype={} rootwait\n",
                 STARTUP_TEMPLATE,
-                os_api.to_linux_path(kernel_path)?.to_string_lossy(),
-                os_api.to_linux_path(initrd_path)?.to_string_lossy(),
+                kernel_path,
+                OSApi::new()?.to_linux_path(initrd_path)?.to_string_lossy(),
                 partuuid,
                 boot_dev.fs_type
             )
