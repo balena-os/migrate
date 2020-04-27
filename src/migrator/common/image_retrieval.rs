f4b57bec6211889f4aef5e40868ccb6d6c4af4f8
Thomas Runte <thomasr@balena.io>
2020-04-27 13:49:57 +1200

    implemented image extraction for flasher images and download progress

--- src/migrator/common/image_retrieval.rs
+++ src/migrator/common/image_retrieval.rs
@@ -1,17 +1,48 @@
+use std::fs::{self, create_dir, remove_dir, File, OpenOptions};
+use std::io::copy;
 use std::path::PathBuf;
 
-use log::{error, info, warn};
+use log::{debug, error, info, warn, Level};
 
 use semver::{Identifier, Version, VersionReq};
 
-use crate::common::{
-    api_calls::{get_os_image, get_os_versions},
-    migrate_info::MigrateInfo,
-    MigErrCtx, MigError, MigErrorKind,
+use crate::common::call;
+use crate::linux::linux_defs::{LOSETUP_CMD, NIX_NONE};
+use crate::{
+    common::{
+        api_calls::{get_os_image, get_os_versions},
+        migrate_info::MigrateInfo,
+        path_append, MigErrCtx, MigError, MigErrorKind,
+    },
+    linux::disk_util::{Disk, PartitionIterator, PartitionReader},
 };
-use failure::ResultExt;
 
-const SUPPORTED_DEVICES: [&str; 1] = ["raspberrypi3"];
+use crate::common::api_calls::Versions;
+use crate::common::stream_progress::StreamProgress;
+use failure::ResultExt;
+use flate2::{Compression, GzBuilder};
+use nix::mount::{mount, umount, MsFlags};
+
+const FLASHER_DEVICES: [&str; 1] = ["intel-nuc"];
+const SUPPORTED_DEVICES: [&str; 2] = ["raspberrypi3", "intel-nuc"];
+
+fn parse_versions(versions: &Versions) -> Vec<Version> {
+    let mut sem_vers: Vec<Version> = versions
+        .versions
+        .iter()
+        .map(|ver_str| Version::parse(ver_str))
+        .filter_map(|ver_res| match ver_res {
+            Ok(version) => Some(version),
+            Err(why) => {
+                error!("Failed to parse version, error: {:?}", why);
+                None
+            }
+        })
+        .collect();
+    sem_vers.sort();
+    sem_vers.reverse();
+    sem_vers
+}
 
 pub(crate) fn download_image(
     mig_info: &mut MigrateInfo,
@@ -20,7 +51,7 @@ pub(crate) fn download_image(
 ) -> Result<PathBuf, MigError> {
     if !SUPPORTED_DEVICES.contains(&device_type) {
         error!(
-            "OS download is not supported for devie type {}",
+            "OS download is not supported for device type {}",
             device_type
         );
         return Err(MigError::displayed());
@@ -29,42 +60,38 @@ pub(crate) fn download_image(
     if let Some(api_key) = mig_info.get_api_key() {
         let api_endpoint = mig_info.get_api_endpoint();
 
-        let mut versions = get_os_versions(&api_endpoint, &api_key, device_type)?;
-        versions.versions.sort();
-        versions.versions.reverse();
+        let versions = get_os_versions(&api_endpoint, &api_key, device_type)?;
 
         let version = match version {
-            "latest" => Version::parse(&versions.latest).context(MigErrCtx::from_remark(
-                MigErrorKind::Upstream,
-                &format!("Failed to parse version from '{}'", versions.latest),
-            ))?,
+            "latest" => {
+                info!("Selected latest version ({}) for download", versions.latest);
+                Version::parse(&versions.latest).context(MigErrCtx::from_remark(
+                    MigErrorKind::Upstream,
+                    &format!("Failed to parse version from '{}'", versions.latest),
+                ))?
+            }
             "default" => {
                 let mut found: Option<Version> = None;
-                for ref ver_str in versions.versions {
-                    match Version::parse(ver_str) {
-                        Ok(cmp_ver) => {
-                            if cmp_ver.is_prerelease() {
-                                continue;
-                            } else {
-                                if cmp_ver
-                                    .build
-                                    .contains(&Identifier::AlphaNumeric("prod".to_string()))
-                                {
-                                    found = Some(cmp_ver);
-                                    break;
-                                }
-                            }
-                        }
-                        Err(why) => {
-                            warn!(
-                                "Failed to parse version from '{}', error: {:?}",
-                                ver_str, why
-                            );
-                            continue;
+                for cmp_ver in parse_versions(&versions) {
+                    debug!("Looking at version {}", cmp_ver);
+                    if cmp_ver.is_prerelease() {
+                        continue;
+                    } else {
+                        if cmp_ver
+                            .build
+                            .contains(&Identifier::AlphaNumeric("prod".to_string()))
+                        {
+                            found = Some(cmp_ver);
+                            break;
                         }
                     }
                 }
+
                 if let Some(found) = found {
+                    info!(
+                        "Selected default version ({}) for download",
+                        found.to_string()
+                    );
                     found
                 } else {
                     error!("No version found for '{}'", version);
@@ -78,29 +105,19 @@ pub(crate) fn download_image(
                         &format!("Failed to parse version from '{}'", version),
                     ))?;
                     let mut found: Option<Version> = None;
-                    for ver_str in &versions.versions {
-                        match Version::parse(ver_str) {
-                            Ok(cmp_ver) => {
-                                if ver_req.matches(&cmp_ver)
-                                    && !cmp_ver.is_prerelease()
-                                    && cmp_ver
-                                        .build
-                                        .contains(&Identifier::AlphaNumeric("prod".to_string()))
-                                {
-                                    found = Some(cmp_ver);
-                                    break;
-                                }
-                            }
-                            Err(why) => {
-                                warn!(
-                                    "Failed to parse version from '{}', error: {:?}",
-                                    ver_str, why
-                                );
-                                continue;
-                            }
+                    for cmp_ver in parse_versions(&versions) {
+                        if ver_req.matches(&cmp_ver)
+                            && !cmp_ver.is_prerelease()
+                            && cmp_ver
+                                .build
+                                .contains(&Identifier::AlphaNumeric("prod".to_string()))
+                        {
+                            found = Some(cmp_ver);
+                            break;
                         }
                     }
                     if let Some(found) = found {
+                        info!("Selected version {} for download", versions.latest);
                         found
                     } else {
                         error!("No version found for '{}'", version);
@@ -113,30 +130,20 @@ pub(crate) fn download_image(
                     ))?;
 
                     let mut found: Option<Version> = None;
-                    for ver_str in &versions.versions {
-                        match Version::parse(ver_str) {
-                            Ok(cmp_ver) => {
-                                if ver_req == cmp_ver
-                                    && !cmp_ver.is_prerelease()
-                                    && (cmp_ver.build == ver_req.build
-                                        || cmp_ver.build.contains(&Identifier::AlphaNumeric(
-                                            "prod".to_string(),
-                                        )))
-                                {
-                                    found = Some(cmp_ver);
-                                    break;
-                                }
-                            }
-                            Err(why) => {
-                                warn!(
-                                    "Failed to parse version from '{}', error: {:?}",
-                                    ver_str, why
-                                );
-                                continue;
-                            }
+                    for cmp_ver in parse_versions(&versions) {
+                        if ver_req == cmp_ver
+                            && !cmp_ver.is_prerelease()
+                            && (cmp_ver.build == ver_req.build
+                                || cmp_ver
+                                    .build
+                                    .contains(&Identifier::AlphaNumeric("prod".to_string())))
+                        {
+                            found = Some(cmp_ver);
+                            break;
                         }
                     }
                     if let Some(found) = found {
+                        info!("Selected version {} for download", versions.latest);
                         found
                     } else {
                         error!("No version found for '{}'", version);
@@ -153,13 +160,199 @@ pub(crate) fn download_image(
 
         // TODO: extract OS image for flasher
 
-        Ok(get_os_image(
-            &api_endpoint,
-            &api_key,
-            device_type,
-            &version.to_string(),
+        let stream = get_os_image(&api_endpoint, &api_key, device_type, &version.to_string())?;
+
+        let img_file_name = path_append(
             &mig_info.work_path.path,
-        )?)
+            &format!(
+                "balena-cloud-{}-{}.img.gz",
+                device_type,
+                version.to_string()
+            ),
+        );
+
+        if FLASHER_DEVICES.contains(&device_type) {
+            let progress = StreamProgress::new(stream, 10, Level::Info);
+            let mut disk = Disk::from_gzip_stream(progress)?;
+            let mut part_iterator = PartitionIterator::new(&mut disk)?;
+            if let Some(part_info) = part_iterator.nth(1) {
+                let mut reader =
+                    PartitionReader::from_part_iterator(&part_info, &mut part_iterator);
+                let extract_file_name = path_append(&mig_info.work_path.path, "root_a.img");
+                let mut tmp_file =
+                    File::create(&extract_file_name).context(MigErrCtx::from_remark(
+                        MigErrorKind::Upstream,
+                        &format!(
+                            "Failed to create temporary file '{}'",
+                            extract_file_name.display()
+                        ),
+                    ))?;
+
+                // TODO: show progress
+                copy(&mut reader, &mut tmp_file).context(MigErrCtx::from_remark(
+                    MigErrorKind::Upstream,
+                    &format!(
+                        "Failed to extract root_a partition to temporary file '{}'",
+                        extract_file_name.display()
+                    ),
+                ))?;
+
+                let cmd_res = call(
+                    LOSETUP_CMD,
+                    &["--show", "-f", &*extract_file_name.to_string_lossy()],
+                    true,
+                )?;
+                let loop_dev = if !cmd_res.status.success() {
+                    error!(
+                        "Failed to loop mount root_a partition from file '{}'",
+                        extract_file_name.display()
+                    );
+                    return Err(MigError::displayed());
+                } else {
+                    cmd_res.stdout
+                };
+                debug!("loop device is '{}'", loop_dev);
+
+                let mount_path = path_append(&mig_info.work_path.path, "mnt_root_a");
+                if !mount_path.exists() {
+                    create_dir(&mount_path).context(MigErrCtx::from_remark(
+                        MigErrorKind::Upstream,
+                        &format!("Failed to create directory '{}'", mount_path.display()),
+                    ))?;
+                }
+
+                debug!("mount path is '{}'", mount_path.display());
+                mount(
+                    Some(loop_dev.as_bytes()),
+                    &mount_path,
+                    Some("ext4".as_bytes()),
+                    MsFlags::empty(),
+                    NIX_NONE,
+                )
+                .context(MigErrCtx::from_remark(
+                    MigErrorKind::Upstream,
+                    &format!(
+                        "Failed to mount '{}' on '{}",
+                        loop_dev,
+                        mount_path.display()
+                    ),
+                ))?;
+
+                let img_path = match device_type {
+                    "intel-nuc" => {
+                        path_append(&mount_path, "opt/resin-image-genericx86-64.resinos-img")
+                    }
+                    _ => {
+                        error!(
+                            "Encountered undefined image name for device type {}",
+                            device_type
+                        );
+                        return Err(MigError::displayed());
+                    }
+                };
+
+                debug!("image path is '{}'", img_path.display());
+
+                {
+                    let mut gz_writer = GzBuilder::new().write(
+                        File::create(&img_file_name).context(MigErrCtx::from_remark(
+                            MigErrorKind::Upstream,
+                            &format!(
+                                "Failed to open image file for writing: '{}'",
+                                img_file_name.display()
+                            ),
+                        ))?,
+                        Compression::best(),
+                    );
+
+                    let mut img_reader = OpenOptions::new().read(true).open(&img_path).context(
+                        MigErrCtx::from_remark(
+                            MigErrorKind::Upstream,
+                            &format!(
+                                "Failed to open image file for reading: '{}'",
+                                img_path.display()
+                            ),
+                        ),
+                    )?;
+
+                    copy(&mut img_reader, &mut gz_writer).context(MigErrCtx::from_remark(
+                        MigErrorKind::Upstream,
+                        &format!(
+                            "Failed to compress image '{}' to '{}'",
+                            img_path.display(),
+                            img_file_name.display()
+                        ),
+                    ))?;
+                }
+
+                debug!(
+                    "The balena OS image was successfully written to '{}'",
+                    img_file_name.display()
+                );
+
+                match umount(&mount_path) {
+                    Ok(_) => {
+                        if let Err(why) = remove_dir(&mount_path) {
+                            warn!(
+                                "Failed to remove mount temporary directory '{}', error: {:?}",
+                                mount_path.display(),
+                                why
+                            );
+                        }
+                    }
+                    Err(why) => {
+                        warn!(
+                            "Failed to unmount temporary mount from '{}', error: {:?}",
+                            mount_path.display(),
+                            why
+                        );
+                    }
+                }
+
+                match call(LOSETUP_CMD, &["-d", &loop_dev], true) {
+                    Ok(cmd_res) => {
+                        if !cmd_res.status.success() {
+                            warn!(
+                                "Failed to remove loop device '{}', stderr: '{}'",
+                                loop_dev, cmd_res.stderr
+                            );
+                        }
+                    }
+                    Err(why) => {
+                        warn!(
+                            "Failed to remove loop device '{}', error: {:?}",
+                            loop_dev, why
+                        );
+                    }
+                };
+
+                if let Err(why) = fs::remove_file(&extract_file_name) {
+                    warn!(
+                        "Failed to remove extracted partition '{}', error: {:?}",
+                        extract_file_name.display(),
+                        why
+                    );
+                }
+            }
+        } else {
+            debug!("Downloading file '{}'", img_file_name.display());
+            let mut file = File::create(&img_file_name).context(MigErrCtx::from_remark(
+                MigErrorKind::Upstream,
+                &format!("Failed to create file: '{}'", img_file_name.display()),
+            ))?;
+
+            // TODO: show progress
+            let mut progress = StreamProgress::new(stream, 10, Level::Info);
+            copy(&mut progress, &mut file).context(MigErrCtx::from_remark(
+                MigErrorKind::Upstream,
+                &format!(
+                    "Failed to write downloaded data to '{}'",
+                    img_file_name.display()
+                ),
+            ))?;
+        }
+
+        Ok(img_file_name)
     } else {
         error!("No api-key found in config.json - unable to retrieve os-image");
         Err(MigError::displayed())
---
+++ src/migrator/common/stream_progress.rs
@@ -0,0 +1,53 @@
+use log::{debug, error, info, trace, warn, Level};
+use std::io::Read;
+use std::time::Instant;
+
+use crate::common::format_size_with_unit;
+
+pub(crate) struct StreamProgress<T> {
+    input: T,
+    bytes_read: u64,
+    last_log: u64,
+    every: u32,
+    level: Level,
+    start_time: Instant,
+}
+
+impl<T: Read> StreamProgress<T> {
+    pub fn new(input: T, every: u32, level: Level) -> StreamProgress<T> {
+        StreamProgress {
+            input,
+            bytes_read: 0,
+            last_log: 0,
+            every,
+            level,
+            start_time: Instant::now(),
+        }
+    }
+}
+
+impl<T: Read> Read for StreamProgress<T> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+        let curr_bytes_read = self.input.read(buf)?;
+        self.bytes_read += curr_bytes_read as u64;
+        let elapsed = Instant::now().duration_since(self.start_time).as_secs();
+        let logs = elapsed / self.every as u64;
+        if logs > self.last_log {
+            self.last_log = logs;
+            let printout = format!(
+                "{} read in {} seconds, @{}/sec ",
+                format_size_with_unit(self.bytes_read),
+                Instant::now().duration_since(self.start_time).as_secs(),
+                format_size_with_unit(self.bytes_read / elapsed),
+            );
+            match self.level {
+                Level::Trace => trace!("{}", printout),
+                Level::Debug => debug!("{}", printout),
+                Level::Warn => warn!("{}", printout),
+                Level::Error => error!("{}", printout),
+                Level::Info => info!("{}", printout),
+            }
+        }
+        Ok(curr_bytes_read)
+    }
+}
